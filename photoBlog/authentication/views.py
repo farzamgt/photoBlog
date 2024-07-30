40ff06540fa1f2d6f59bae15d56d81e10c741b55
Farzam Farin <farzamgt@gamil.com>
2024-07-30 13:30:02 +0300

    Updated authentication app views and added custom filters for styling HTML forms.

--- photoBlog/authentication/views.py
+++ photoBlog/authentication/views.py
@@ -1,17 +1,17 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth import login as auth_login
-from django.contrib.auth.forms import  AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.decorators import login_required
 from .forms import CustomUserCreationForm
 from posts.models import Category
+from django.core.exceptions import ValidationError
 
 
 def homepage(request):
-    categories = Category.objects.all()  # Fetch all categories
+    categories = Category.objects.all()  
     return render(request, 'homepage.html', {
         'user': request.user,
-        'categories': categories,  # Pass categories to the template
+        'categories': categories, 
     })
 
 def login_view(request):
@@ -19,9 +19,9 @@ def login_view(request):
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
-            auth_login(request, user)
+            login(request, user)
             next_url = request.POST.get('next', 'homepage')
-            if not next_url or next_url == request.path:  # Handle empty or same path case
+            if not next_url or next_url == request.path:
                 next_url = 'homepage'
             return redirect(next_url)
     else:
@@ -42,6 +42,16 @@ def register_view(request):
             user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('homepage')
+        else:
+            errors = []
+            if 'username' in form.errors:
+                errors.append('Username is already taken. Please choose a different one.')
+            if 'password2' in form.errors:
+                errors.append('Password must be at least 8 characters long and include numbers and special characters.')
+            return render(request, 'authentication/registration.html', {
+                'form': form,
+                'errors': errors
+            })
     else:
         form = CustomUserCreationForm()
     return render(request, 'authentication/registration.html', {'form': form})
